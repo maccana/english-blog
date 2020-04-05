db9c2bed81453081546f235e2c31ca0683239c67
Mike <mikeymccann@gmail.com>
2020-04-05 19:46:04 -0400

    Configuration for blog path

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `markdown-pages`,
-        path: `${__dirname}/content/posts`,
+        path: `${__dirname}/blog`,
       },
     },
     {
